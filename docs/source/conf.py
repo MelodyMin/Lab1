6079f384c0985399e0f41f0c89d1b3210794b446
MelodyMin <104144380+MelodyMin@users.noreply.github.com>
2022-05-02 16:48:43 +0800

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,8 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Lab 1: Dependency Analysis and Dependency Graphs'
+author = '刘奕秀 李敏吴佩媛'
 
 release = '0.1'
 version = '0.1.0'
